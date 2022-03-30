e8cbb15aa4b8685928628e1a3348ea85cefaa859
hkalife <henriquekalife@gmail.com>
2022-03-30 19:23:53 -0300

    feat: create data

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { ApolloServer } from 'apollo-server';
 import { typeDefs } from './schema';
-import { Query } from './resolvers';
+import { Query, Mutation } from './resolvers';
 import { PrismaClient, Prisma } from '@prisma/client';
 
 const prisma = new PrismaClient();
@@ -13,6 +13,7 @@ const server = new ApolloServer({
   typeDefs,
   resolvers: {
     Query,
+    Mutation,
   },
   context: {
     prisma
